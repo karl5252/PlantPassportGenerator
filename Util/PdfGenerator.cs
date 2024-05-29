6fbe6f2f67d32df0d9b92e9637972b0cf530470e
Karol Michalik <MICHAK14@heiway.net>
2024-05-29 14:51:43 +0200

    adjusmtenfts for pdf generator

--- Util/PdfGenerator.cs
+++ Util/PdfGenerator.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using PdfSharp.Drawing;
@@ -6,16 +6,20 @@ using PdfSharp.Pdf;
 using System.Windows;
 using PlantPassportGenerator;
 using WpfApp1.Model;
+using System.IO;
 
 public class PdfGenerator
 {
     private List<PlantPassport> _plantPassports;
     private List<BasketItem> _basket;
+    private XImage _euFlag;
 
     public PdfGenerator(List<PlantPassport> plantPassports, List<BasketItem> basket)
     {
         _plantPassports = plantPassports;
         _basket = basket;
+        string euFlagPath = Path.Combine(Directory.GetCurrentDirectory(), "Images", "EU.png");
+        _euFlag = XImage.FromFile(euFlagPath);
     }
 
    /* public PdfGenerator(List<PlantPassport> plantPassports, List<BasketItem> basket)
@@ -32,7 +36,7 @@ public class PdfGenerator
         PdfDocument document = new PdfDocument();
         document.Info.Title = "Plant Passports";
 
-        int passportsPerPage = 15; // 5 rows * 3 columns
+        int passportsPerPage = 10; // 5 rows * 3 columns
         int passportCounter = 0;
 
         PdfPage page = null;
@@ -54,21 +58,22 @@ public class PdfGenerator
                     page.Size = PdfSharp.PageSize.A4;
                     gfx = XGraphics.FromPdfPage(page);
                 }
-
                 int row = (passportCounter % passportsPerPage) / 3;
-                int column = (passportCounter % passportsPerPage) % 3;
+                int column = (passportCounter % passportsPerPage) % 2;
 
-                double x = 40 + column * (210 + 20); // 2.5cm (around 70 points) + 5cm (around 140 points)
-                double y = 40 + row * (100 + 20); // 2.5cm (around 70 points) + 5cm (around 140 points)
+                double x = 40 + column * (230 + 30); // 2.5cm (around 70 points) + 5cm (around 140 points) + margin
+                double y = 40 + row * (100 + 55); // 2.5cm (around 70 points) + 5cm (around 140 points) + margin
+                
+                gfx.DrawImage(_euFlag, x, y, 80, 50); // Draw flag
 
-                gfx.DrawString("EU", boldFont, XBrushes.Black, new XRect(x, y, 70, 20), XStringFormats.TopLeft);
-                gfx.DrawString("Paszport roslin / Plant passport", font, XBrushes.Black, new XRect(x + 70, y, 140, 20), XStringFormats.TopLeft);
+                gfx.DrawString("Paszport roślin / Plant passport", font, XBrushes.Black, new XRect(x + 85, y, 140, 20), XStringFormats.TopLeft);
 
-                gfx.DrawString($"A: {passport.PlantName}", font, XBrushes.Black, new XRect(x, y + 20, 210, 20), XStringFormats.TopLeft);
-                gfx.DrawString($"B: {passport.Id}", font, XBrushes.Black, new XRect(x, y + 40, 210, 20), XStringFormats.TopLeft);
-                gfx.DrawString($"C: {passport.Sector} / {DateTime.Now.Year}", font, XBrushes.Black, new XRect(x, y + 60, 210, 20), XStringFormats.TopLeft);
-                gfx.DrawString("D: PL", font, XBrushes.Black, new XRect(x, y + 80, 210, 20), XStringFormats.TopLeft);
+                gfx.DrawString($"A {passport.PlantName}", font, XBrushes.Black, new XRect(x + 5, y + 70, 210, 20), XStringFormats.TopLeft);
+                gfx.DrawString($"B {passport.Id}", font, XBrushes.Black, new XRect(x + 5, y + 90, 210, 20), XStringFormats.TopLeft);
+                gfx.DrawString($"C {passport.Sector} /  {DateTime.Now.ToString("yy")}", font, XBrushes.Black, new XRect(x + 5, y + 110, 210, 20), XStringFormats.TopLeft);
+                gfx.DrawString("D PL", font, XBrushes.Black, new XRect(x + 5, y + 130, 210, 20), XStringFormats.TopLeft);
 
+                gfx.DrawRectangle(new XPen(XColors.Black, 1), x, y, 245, 150); // Draw border 
                 passportCounter++;
             }
         }
